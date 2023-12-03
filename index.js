b4cc36e922c4ab1b29de19be48cdbb890b1c1221
isidorostsa <tsa.isidoros@gmail.com>
2023-12-03 14:52:56 +0200

    export app

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var http = require('http');
 
 var oas3Tools = require('oas3-tools');
 var serverPort = process.env.PORT || 8080;
-// Dummy comment for a test commit
 
 // swaggerRouter configuration
 var options = {
@@ -18,8 +17,11 @@ var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/open
 var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-});
+if (process.env.NODE_ENV !== "test") {
+    http.createServer(app).listen(serverPort, function () {
+        console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+        console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    });
+}
 
+module.exports = app;
