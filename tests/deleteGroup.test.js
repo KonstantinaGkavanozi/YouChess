7983104dd57298f5743830c37ef4496f3d63636c
isidorostsa <tsa.isidoros@gmail.com>
2023-12-17 23:48:54 +0200

    group/delete endpoint tests

---
+++ tests/deleteGroup.test.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const test = require('ava');
+const listen = require('test-listen');
+const got = require('got');
+
+const { deleteGroup } = require('../service/GroupService.js');
+const app = require('../index.js');
+
+test.before(async (t) => {
+    t.context.server = http.createServer(app);
+    t.context.prefixUrl = await listen(t.context.server);
+    t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json' });
+});
+
+test.after.always((t) => {
+    t.context.server.close();
+});
+
+test('test to pass', (t)=> {
+    t.pass();
+});
+
+test('Delete group by function', async (t) => {
+    await deleteGroup(10);
+    t.is(true, true);
+});
+
+test('Delete group', async (t) => {
+    const groupID = 10;
+    const {body, statusCode} = await t.context.got.delete(`group/${groupID}`);
+
+    t.is(statusCode, 200);
+});
+
+test('Delete group Bad Request', async (t) => {
+    const res = await t.throwsAsync(t.context.got.delete('group/abc'));
+
+    t.is(res.message, 'Response code 400 (Bad Request)');
+});
